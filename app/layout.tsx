a42cfab24b1ce15fab49384c2a6bb229f18dbd65
Paul4240 <pauldecarlis4@gmail.com>
2026-02-01 14:58:15 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,18 @@
-/* layout.tsx */
-
-import './globals.css'
+import './globals.css';
 
 export const metadata = {
   title: 'Atlas Digital Lab',
   description: 'Atlas Digital Lab Website',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }

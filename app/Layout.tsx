b60f9cb30fa1a2a1f5cddba1b00bfdf773b9b2b7
Paul4240 <pauldecarlis4@gmail.com>
2026-02-01 15:09:08 -0600

    Create Layout.tsx

---
+++ app/Layout.tsx
@@ -0,0 +1,18 @@
+import './globals.css';
+
+export const metadata = {
+  title: 'Atlas Digital Lab',
+  description: 'Atlas Digital Lab Website',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
